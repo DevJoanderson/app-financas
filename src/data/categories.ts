797d82945bd8953683e005fe77e8722e5b810f0f
DevJoanderson <joandersonsouza410@gmail.com>
2025-04-18 23:59:37 -0300

    feat: cria types para Category e Item, e dados de exemplo para ambos

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from "../types/Category";
+
+export const categories: Category = {
+    food: { title: 'Alimentação', color:' #FFA500', expense: true},
+    rent: { title: 'Aluguel', color: '#FF0000', expense: true},
+    salary: { title: 'Salário', color: '#4CAF50', expense: false},
+}
---
+++ src/data/items.ts
@@ -0,0 +1,10 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  { date: new Date(2025, 4, 7), category: 'rent', title: 'Aluguel Apt', value: 900  },
+  { date: new Date(2025, 4, 18), category: 'food', title: 'McDonalds', value: 39.90 },
+  { date: new Date(2025, 4, 18), category: 'food', title: 'Burger King', value: 34.90 },
+  { date: new Date(2025, 4, 28), category: 'rent', title: 'Academia', value: 140 },
+  { date: new Date(2025, 4, 28), category: 'rent', title: 'Dentista', value: 110 },
+  { date: new Date(2025, 5, 4), category: 'salary', title: 'Salário FirsByte', value: 4540 },
+];
