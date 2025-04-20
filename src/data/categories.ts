7995b1b425174dee26c5b819031d79c2d93e4bb1
DevJoanderson <joandersonsouza410@gmail.com>
2025-04-20 18:57:04 -0300

    feat(tabela): filtro de busca finalizado

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,5 +3,6 @@ import { Category } from "../types/Category";
 export const categories: Category = {
     food: { title: 'Alimentação', color:' #FFA500', expense: true},
     rent: { title: 'Aluguel', color: '#FF0000', expense: true},
+    investment:{title: 'Saude', color: '#5DADE2', expense: true},
     salary: { title: 'Salário', color: '#4CAF50', expense: false},
 }
