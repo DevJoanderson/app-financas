cb8a012ece9c9449467e8028c950ec912d053d5d
DevJoanderson <joandersonsouza410@gmail.com>
2025-04-21 01:31:57 -0300

    feat: cria componente InfoArea com setas para navegar entre meses e formatação do mês atual

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { categories } from './data/categories';
 import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 import { TableArea } from './components/TableArea'
+import { InfoArea } from './components/InfoArea'; // ajuste o caminho se estiver diferente
+
+
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
@@ -15,12 +18,19 @@ const App = () => {
     setFilteredList(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]);
 
+  const handleMontChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
   return <C.Container>
     <C.Header>
       <C.HeaderText>Sistema Finaceiro</C.HeaderText>
     </C.Header>
     <C.Body>
-      {/* Aréa de informações */}
+        <InfoArea 
+        currentMonth={currentMonth}
+        onMonthChange={handleMontChange}
+        />
 
        {/* Aréa de inserção */}
 
